4ad11a5e2128bce7d628c2cae1feb4fa60e81fd1
chedly-abdennadher <abdennadher.chedly@gmail.com>
2019-07-09 10:28:59 +0100

    fichiers suppris

--- lectureecriturefichier.php
+++
@@ -1,9 +0,0 @@
-<?php
-$retourligne="".PHP_EOL;
-$nomfichier="essai.txt";
-$ressource=fopen ($nomfichier,'r+');
-$contenufichier=file_get_contents ($nomfichier);
-echo ($contenufichier);
-fclose($ressource); 
-
-?>
