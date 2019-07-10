67d4af9afd9258ea67dee295dec86a4f385ec179
chedly-abdennadher <abdennadher.chedly@gmail.com>
2019-07-10 14:38:58 +0100

    essai

---
+++ src/parts/information générale.php
@@ -0,0 +1,9 @@
+<div class="col-3 border-left"> 
+<div>
+<img src ="web/favicon/index.jpeg">
+</div>
+<div class="row"> 
+<?php include "identité.php"?>
+</div>
+<?php include "SGBDlangueprojet.php"?>
+</div>
